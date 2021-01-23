64539475b15071563dabc4437033ca2ddeee9524
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-23 10:22:50 -0800

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -16,7 +16,35 @@ async function loadPokemons() {
         .then(function(trainers) {
             trainers.forEach(function(trainer) {
                 let div = document.createElement('div');
-                div.innerHTML = trainer.name;
+                div.setAttribute('class', 'card');
+                div.setAttribute('data-id', trainer.id)
+                
+                let p = document.createElement('p');
+                p.innerText = trainer.name
+
+                let btn = document.createElement('button')
+                btn.setAttribute("data-trainer-id", trainer.id)
+                btn.innerText = "Add Pokemon"
+                btn.addEventListener("click", (event))
+
+                let ul = document.createElement('ul')
+                let pokemons = trainer.pokemons
+                pokemons.forEach(renderPokemons);
+                function renderPokemons(nickname, species) {
+                    let li = document.createElement('li');
+                    li.innerText = pokemons.nickname + pokemons.species
+
+                    let releaseBtn = document.createElement('button')
+                    releaseBtn.setAttribute('class', "release")
+                    releaseBtn.setAttribute("data-pokemon-id", pokemons.id)
+                    releaseBtn.innerText = "Release"
+                    releaseBtn.addEventListener("click", (event))
+
+                    ul.appendChild(li, releaseBtn)
+                }
+
+                div.append(p, btn, ul)
+
                 trainerContainer.appendChild(div);
             })
         })
